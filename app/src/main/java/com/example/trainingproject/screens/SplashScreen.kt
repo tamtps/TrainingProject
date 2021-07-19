11480561014d80027dd750e4f06edb12a7f840c1
Namesiss <45970741+Namesiss@users.noreply.github.com>
2021-07-19 11:59:59 +0700

    Training

---
+++ app/src/main/java/com/example/trainingproject/screens/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.trainingproject.screens
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.trainingproject.MainActivity
+import com.example.trainingproject.R
+
+class SplashScreen : AppCompatActivity() {
+    lateinit var handler : Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
