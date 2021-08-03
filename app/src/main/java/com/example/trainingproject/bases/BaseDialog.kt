2df2cc4fdd5def992ac40cf4a7881b46dea9c8dc
Phan Hai Long <phanhailong12345678@gmail.com>
2021-08-03 17:06:08 +0700

    base_activity

--- app/src/main/java/com/example/trainingproject/bases/BaseDialog.kt
+++ app/src/main/java/com/example/trainingproject/bases/BaseDialog.kt
@@ -6,39 +6,34 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import com.example.trainingproject.R
+import com.example.trainingproject.databinding.DialogSimpleBinding
 
 open class BaseDialog(context: Context) : Dialog(context) {
-    lateinit var title : TextView
-    lateinit var content : TextView
-    lateinit var buttonOK : Button
-    lateinit var buttonCancel : Button
+    lateinit var binding: DialogSimpleBinding
 
     fun setContentView() {
-        super.setContentView(R.layout.dialog_simple)
-        title = findViewById(R.id.dialog_title)
-        content = findViewById(R.id.dialog_content)
-        buttonOK = findViewById(R.id.btn_yes)
-        buttonCancel = findViewById(R.id.btn_no)
+        binding = DialogSimpleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun errorDialog(string : String?){
-        title.text = context.getString(R.string.error)
-        content.text = string
+        binding.dialogTitle.text = context.getString(R.string.error)
+        binding.dialogContent.text = string
         showCancelButton(false)
         onOKDismiss()
         show()
     }
 
     open fun showCancelButton(boolean: Boolean){
-        if (boolean)   buttonCancel.visibility = View.VISIBLE
-        else buttonCancel.visibility = View.INVISIBLE
+        if (boolean)   binding.btnNo.visibility = View.VISIBLE
+        else binding.btnNo.visibility = View.INVISIBLE
     }
 
     fun onCancelDismiss(){
-        buttonCancel.setOnClickListener(View.OnClickListener { dismiss() })
+        binding.btnNo.setOnClickListener(View.OnClickListener { dismiss() })
     }
     fun onOKDismiss(){
-        buttonOK.setOnClickListener(View.OnClickListener { dismiss() })
+        binding.btnYes.setOnClickListener(View.OnClickListener { dismiss() })
     }
 
 }
--- app/src/main/java/com/example/trainingproject/screens/HowToVideoActivity.kt
+++ app/src/main/java/com/example/trainingproject/screens/HowToVideoActivity.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,16 +14,15 @@ import com.example.trainingproject.api.RetrofitClient
 import com.example.trainingproject.bases.BaseActivity
 import com.example.trainingproject.bases.BaseDialog
 import com.example.trainingproject.components.HowToVideoAdapter
+import com.example.trainingproject.databinding.ActivityHowToVideoBinding
 import com.example.trainingproject.models.Content
 import com.example.trainingproject.models.Response
 import com.example.trainingproject.models.Videos
 import retrofit2.Call
 import retrofit2.Callback
 
-class HowToVideoActivity : BaseActivity() {
+class HowToVideoActivity : BaseActivity<ActivityHowToVideoBinding>() {
     var list : ArrayList<Videos> = ArrayList()
-    lateinit var progressCircular : ProgressBar
-    lateinit var listHowToVideos : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,17 +30,9 @@ class HowToVideoActivity : BaseActivity() {
         centerText(getString(R.string.appbar_how_to_video))
         rightIcon(R.drawable.btn_home_white)
 
-        val prefs : SharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE)
-        val deviceId = prefs.getString("deviceId","")
+        binding.imgLeft.setOnClickListener(View.OnClickListener { finish() })
 
-        progressCircular = findViewById(R.id.progress_circular)
-        listHowToVideos = findViewById(R.id.list_how_to_videos)
-
-        leftIcon.setOnClickListener(View.OnClickListener {
-            finish()
-        })
-
-        rightIcon.setOnClickListener(View.OnClickListener {
+        binding.imgRight.setOnClickListener(View.OnClickListener {
             startActivity(Intent(this, MainScreen::class.java))
             finish()
         })
@@ -48,11 +40,9 @@ class HowToVideoActivity : BaseActivity() {
         getVideoAPI("1")
     }
 
-    override fun getBodyLayout(): Int {
-        return R.layout.activity_how_to_video
-    }
-
-    override fun hasDrawer(): Boolean {return false }
+    override fun getViewBinding(): ActivityHowToVideoBinding = ActivityHowToVideoBinding.inflate(layoutInflater)
+    override fun getBodyLayout(): Int = R.layout.activity_how_to_video
+    override fun hasDrawer(): Boolean = false
 
     fun getVideoAPI(category: String){
         RetrofitClient().instance.getVideo(category)
@@ -61,11 +51,11 @@ class HowToVideoActivity : BaseActivity() {
                     call: Call<Response<Content>>,
                     response: retrofit2.Response<Response<Content>>
                 ) {
-                    progressCircular.visibility = View.INVISIBLE
+                    bindingBody.progressCircular.visibility = View.INVISIBLE
                     list.addAll(response.body()!!.result.content)
-                    listHowToVideos.adapter = HowToVideoAdapter(applicationContext, list)
-                    listHowToVideos.layoutManager = LinearLayoutManager(applicationContext)
-                    listHowToVideos.addItemDecoration(DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL))
+                    bindingBody.listHowToVideos.adapter = HowToVideoAdapter(applicationContext, list)
+                    bindingBody.listHowToVideos.layoutManager = LinearLayoutManager(applicationContext)
+                    bindingBody.listHowToVideos.addItemDecoration(DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL))
                 }
 
                 override fun onFailure(call: Call<Response<Content>>, t: Throwable) {
@@ -75,5 +65,4 @@ class HowToVideoActivity : BaseActivity() {
                 }
             })
     }
-
 }
